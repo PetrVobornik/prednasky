978f72ea62e18c7be8e6ca481e55ed69531f4aaa
Petr Voborník <vobornik@outlook.cz>
2019-05-18 21:16:06 +0200

    Asteroids.Android

---
+++ Xamarin.Forms/09-Hra/Asteroids/Asteroids.Android/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Microsoft.Xna.Framework;
+using Android.Content.PM;
+using Asteroids.Shared;
+using Android.Views;
+
+namespace Asteroids.Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true,
+        AlwaysRetainTaskState =true, LaunchMode = LaunchMode.SingleInstance,
+        ScreenOrientation = ScreenOrientation.FullUser,
+        ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard |
+        ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize  | ConfigChanges.ScreenLayout)]
+    public class MainActivity : AndroidGameActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+
+            var g = new GameAsteroids();
+            SetContentView((View)g.Services.GetService(typeof(View)));
+            g.Run();
+        }
+        //public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        //{
+        //    Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //}
+    }
+}
